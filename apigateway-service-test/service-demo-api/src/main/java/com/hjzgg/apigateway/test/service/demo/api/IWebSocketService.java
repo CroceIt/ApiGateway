41bdaf9ca98789b3d7feb8fdb2ba8c32f80c6397
hujunzheng <hujunzheng@cmos.chinamobile.com>
2018-09-25 09:54:27 +0800

    修改websocket 测试类·

--- apigateway-service-test/service-demo-api/src/main/java/com/hjzgg/apigateway/test/service/demo/api/IWebSocketService.java
+++ apigateway-service-test/service-demo-api/src/main/java/com/hjzgg/apigateway/test/service/demo/api/IWebSocketService.java
@@ -1,8 +1,26 @@
 package com.hjzgg.apigateway.test.service.demo.api;
 
+import com.alibaba.dubbo.config.annotation.Service;
+
+import javax.websocket.*;
+import javax.websocket.server.ServerEndpoint;
+
 /**
  * @author hujunzheng
  * @create 2018-08-26 15:37
  **/
+@ServerEndpoint(value = "/ws")
+@Service(version = "iWebSocket", group = "hjzgg")
 public interface IWebSocketService {
+    @OnOpen
+    void onOpen(Session session);
+
+    @OnClose
+    void onClose(Session session);
+
+    @OnMessage
+    void OnMessage(Session session, String message);
+
+    @OnError
+    void onError(Session session, Throwable throwable);
 }
--- apigateway-service-test/service-demo/src/main/java/com/hjzgg/apigateway/test/service/demo/WebSocketServiceImpl.java
+++ apigateway-service-test/service-demo/src/main/java/com/hjzgg/apigateway/test/service/demo/WebSocketServiceImpl.java
@@ -1,37 +1,31 @@
-package com.hjzgg.apigateway.websocket.test.service;
+package com.hjzgg.apigateway.test.service.demo;
 
-import com.hjzgg.apigateway.api.websocket.WebSocketServiceAdapter;
-import org.springframework.stereotype.Component;
+import com.alibaba.dubbo.config.annotation.Service;
+import com.hjzgg.apigateway.test.service.demo.api.IWebSocketService;
 
 import javax.websocket.*;
-import javax.websocket.server.ServerEndpoint;
 
 /**
  * @author hujunzheng
  * @create 2018-08-20 11:55
  **/
-@Component
-@ServerEndpoint(value = "/ws")
-public class WebSocketServiceImpl extends WebSocketServiceAdapter {
-    @OnOpen
+@Service
+public class WebSocketServiceImpl implements IWebSocketService {
     @Override
     public void onOpen(Session session) {
         System.out.println(session.getId() + " is open");
     }
 
-    @OnClose
     @Override
     public void onClose(Session session) {
         System.out.println(session.getId() + " is close");
     }
 
-    @OnMessage
     @Override
     public void OnMessage(Session session, String message) {
         System.out.println(session.getId() + " message is " + message);
     }
 
-    @OnError
     @Override
     public void onError(Session session, Throwable throwable) {
         System.out.println(session.getId() + " is error, " + throwable.getMessage());
